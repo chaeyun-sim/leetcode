de528662e028924af038394f4f51f9d94c2875db
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-08-06 16:41:45 +0900

    Time: 53 ms (69.38%), Space: 50.8 MB (49.19%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    s = s.trim()
+    const arr = s.split(' ')
+    const result = []
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] != '') result.push(arr[i])
+    }
+
+    return result.join(' ')
+};
